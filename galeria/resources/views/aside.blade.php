d460aaf916cbb82ebfefc91cfbdc9eb2b28d81ce
benja <benjavallory@hotmail.com>
2024-01-09 18:12:59 -0300

    comit

--- galeria/resources/views/aside.blade.php
+++
@@ -1,9 +0,0 @@
-<aside class="w-72 bg-gray-900 text-white">
-    <ul class="ml-10 mt-20">
-        <li class="font-thin mt-16 mb-16"><a href="/pr">Paises recorridos</a></li>
-        <li class="font-thin mt-16 mb-16"><a href="">Agregar Foto</a></li>
-        <li class="font-thin mt-16 mb-16"><a href="">Lugares visitados</a></li>
-        <li class="font-thin mt-16 mb-16"><a href="">Personas conocidas</a></li>
-        <li class="font-thin mt-16 mb-16"><a href="">Culturas descubiertas</a></li>
-    </ul>
-</aside>
